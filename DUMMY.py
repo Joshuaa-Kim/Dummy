f369c33f6a9bd364140183f6af21de7e69d106f8
Joshua Kim <jkim2224@usc.edu>
2023-08-28 14:34:52 -0700

    Edits from newedits branch

--- DUMMY.py
+++ DUMMY.py
@@ -1,1 +1,4 @@
 print("This edit was made in main branch")
+
+print("These edits are made from newedits, check it out bro!")
+print("Praise the Lord Almighty regardless of situation, even when things get tough walk with Him and he will make your paths straight")
